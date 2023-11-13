bfe12e52682b8b2dc28bffcd1fc752c9758dead1
Maiko Tan <maiko.tan.coding@gmail.com>
2023-11-13 17:15:45 +0800

    refactor: move channel api to server side

--- pkgs/nitori_core/lib/src/channel.dart
+++ pkgs/nitori_core/lib/src/channel.dart
@@ -2,8 +2,6 @@ import 'dart:convert';
 import 'dart:async';
 import 'dart:io';
 
-import 'package:nitori_core/nitori_core.dart';
-
 enum ChannelType {
   TEXT(0),
   VOICE(1),
@@ -63,23 +61,4 @@ abstract interface class ChannelInterface {
 
   /// Delete Channel
   Future<void> deleteChannel(String id);
-}
-
-class ChannelController extends BaseController<ChannelInterface> {
-  ChannelController(ChannelInterface _interface) : super(_interface);
-
-  @override
-  Future<void> handlePost(HttpRequest request) async {
-    final body = await request
-        .cast<List<int>>()
-        .transform(utf8.decoder)
-        .transform(json.decoder)
-        .single as Map<String, dynamic>;
-
-    final channel = Channel.fromJson(body);
-
-    request.response
-      ..statusCode = HttpStatus.ok
-      ..write(json.encode(channel.toJson()));
-  }
 }
---
+++ pkgs/nitori_server/lib/src/channel.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:nitori_core/nitori_core.dart';
+
+import './base.dart';
+
+class ChannelController extends BaseController<ChannelInterface> {
+  @override
+  var prefix = '/channel';
+  ChannelController(ChannelInterface _interface) : super(_interface);
+
+  @override
+  Future<void> handlePost(HttpRequest request) async {
+    final body = await request
+        .cast<List<int>>()
+        .transform(utf8.decoder)
+        .transform(json.decoder)
+        .single as Map<String, dynamic>;
+
+    final channel = Channel.fromJson(body);
+
+    request.response
+      ..statusCode = HttpStatus.ok
+      ..write(json.encode(channel.toJson()));
+  }
+}
