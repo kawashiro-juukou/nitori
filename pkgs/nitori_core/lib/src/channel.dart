ac30c17809597ae4e09a8d799a0df4b9af43153e
Maiko Tan <maiko.tan.coding@gmail.com>
2023-11-13 15:28:55 +0800

    feat: add base controller for channel

--- pkgs/nitori_core/lib/src/channel.dart
+++ pkgs/nitori_core/lib/src/channel.dart
@@ -1,3 +1,9 @@
+import 'dart:convert';
+import 'dart:async';
+import 'dart:io';
+
+import 'package:nitori_core/nitori_core.dart';
+
 enum ChannelType {
   TEXT(0),
   VOICE(1),
@@ -22,6 +28,26 @@ class Channel {
   String? parentId;
 
   Channel(this.id, this.type, this.name, this.parentId);
+
+  /// Static method to create a Channel instance from JSON
+  static Channel fromJson(Map<String, dynamic> json) {
+    return Channel(
+      json['id'],
+      ChannelType.values[json['type']],
+      json['name'],
+      json['parentId'],
+    );
+  }
+
+  /// Instance method to convert the Channel instance to JSON
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'type': type,
+      'name': name,
+      'parentId': parentId,
+    };
+  }
 }
 
 abstract interface class ChannelInterface {
@@ -37,4 +63,23 @@ abstract interface class ChannelInterface {
 
   /// Delete Channel
   Future<void> deleteChannel(String id);
+}
+
+class ChannelController extends BaseController<ChannelInterface> {
+  ChannelController(ChannelInterface _interface) : super(_interface);
+
+  @override
+  Future<void> handlePost(HttpRequest request) async {
+    final body = await request
+        .cast<List<int>>()
+        .transform(utf8.decoder)
+        .transform(json.decoder)
+        .single as Map<String, dynamic>;
+
+    final channel = Channel.fromJson(body);
+
+    request.response
+      ..statusCode = HttpStatus.ok
+      ..write(json.encode(channel.toJson()));
+  }
 }
