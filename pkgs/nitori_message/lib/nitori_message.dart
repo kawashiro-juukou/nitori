3e4aa04fa6cc66fdd38c746f166aa20f0c0f7368
Maiko Tan <maiko.tan.coding@gmail.com>
2023-11-14 15:50:45 +0800

    fix: exports for elements

--- pkgs/nitori_message/lib/nitori_message.dart
+++ pkgs/nitori_message/lib/nitori_message.dart
@@ -2,3 +2,7 @@ library;
 
 export 'src/base.dart';
 export 'src/elements/basic.dart';
+export 'src/elements/layout.dart';
+export 'src/elements/meta.dart';
+export 'src/elements/modifier.dart';
+export 'src/elements/resource.dart';
